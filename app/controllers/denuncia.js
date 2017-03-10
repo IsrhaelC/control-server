7313abf8618ba0e68946aa1e4344cac13e42764f
Isrhael Christian <isrhael.christian@hotmail.com>
2017-03-10 11:32:23 -0300

    Limpeza de dados

--- app/controllers/denuncia.js
+++ app/controllers/denuncia.js
@@ -1,11 +1,4 @@
-var denuncias = [
-    {_id: 1, categoria: "Segurança", desc: 'Descrição', idAutor: 1, date: '06/08/1994', cidade: 'Fortaleza', end: 'Rua Livreiro Arlindo, 275'},
-    {_id: 2, categoria: "Educação", desc: 'Descrição', idAutor: 2, date: '06/08/1994', cidade: 'Fortaleza', end: 'Rua Livreiro Arlindo, 300'},
-    {_id: 3, categoria: "Saude", desc: 'Descrição', idAutor: 2, date: '06/08/1994', cidade: 'Fortaleza', end: 'Rua Livreiro Arlindo, 80'},
-    {_id: 4, categoria: "Acessibilidade", desc: 'Descrição', idAutor: 3, date: '06/08/1994', cidade: 'Fortaleza', end: 'Rua Livreiro Arlindo, 120'},
-    {_id: 5, categoria: "Desperdicio-agua", desc: 'Descrição', idAutor: 4, date: '06/08/1994', cidade: 'Fortaleza', end: 'Rua Livreiro Arlindo, 10'},
-    {_id: 6, categoria: "Desperdicio-energia", desc: 'Descrição', idAutor: 5, date: '06/08/1994', cidade: 'Fortaleza', end: 'Rua Livreiro Arlindo, 400'}
-];
+var denuncias = [];
 
 
 module.exports = function () {
